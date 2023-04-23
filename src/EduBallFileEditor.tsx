d00c927b722eb21921498bea761ab5f43a541dfd
Stefan Dausend-Werner <sdausend-werner@plansoft.de>
2023-04-23 13:55:52 +0200

    refactoring

--- src/EduBallFileEditor.tsx
+++ src/EduBallFileEditor.tsx
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { produce } from "immer";
-import { QuestionList } from "./QuestionList";
-import { StudentList } from "./StudentList";
 import { generateTxtFile } from "./generateTxtFile";
-import { IConfig, IQuestions, toggleDirections } from "./interfaces";
+import { IConfig, IQuestions } from "./interfaces";
 import { Container } from "@mui/material";
-import { LoadQuestions } from "./LoadQuestions";
-import { LoadTeams } from "./LoadTeams";
 import {
   locallyLoadData,
   locallySaveEditorData,
 } from "./localLoadingFunctions";
+import { StudentEditorView } from "./StudentEditorView";
+import { QuestionEditorView } from "./QuestionEditorView";
 
 export function EduBallFileEditor({
   loadedConfig,
@@ -45,140 +42,22 @@ export function EduBallFileEditor({
     }
   }, [teamBlue, teamRed, questions, dataLoaded]);
 
-  function deleteStudent(teamSetter: Function) {
-    return function (index: number) {
-      teamSetter((baseState: string[]) => {
-        const nextState = produce(baseState, (draftState: string[]) => {
-          draftState.splice(index, 1);
-        });
-        return nextState;
-      });
-    };
-  }
-
-  function addStudent(teamSetter: Function, studentName: string = "") {
-    teamSetter((prev: string[]) => [...prev, studentName]);
-    console.debug(teamBlue);
-  }
-
-  function toggleStudentTeam(index: number, direction: toggleDirections) {
-    function toggle(
-      index: number,
-      fromTeam: string[],
-      fromTeamSetter: Function,
-      toTeamSetter: Function
-    ) {
-      addStudent(toTeamSetter, fromTeam[index]);
-      deleteStudent(fromTeamSetter)(index);
-    }
-    switch (direction) {
-      case "fromBlueToRed":
-        toggle(index, teamBlue, setTeamBlue, setTeamRed);
-        break;
-      case "fromRedToBlue":
-        toggle(index, teamRed, setTeamRed, setTeamBlue);
-        break;
-      default:
-        throw new Error("Unknown student change direction");
-    }
-  }
-
-  //// ---------------------------------------------------------------------------------------------
-  //// TODO deduplicate and rename
-  function changeTeamBlue(studentname: string, index: number) {
-    setTeamBlue((baseState) => {
-      const nextState = produce(baseState, (draftState: string[]) => {
-        draftState[index] = studentname;
-      });
-      return nextState;
-    });
-  }
-  function changeTeamRed(studentname: string, index: number) {
-    setTeamRed((baseState) => {
-      const nextState = produce(baseState, (draftState: string[]) => {
-        draftState[index] = studentname;
-      });
-      return nextState;
-    });
-  }
-  //// ---------------------------------------------------------------------------------------------
-
-  function addQuestion() {
-    setQuestions((prev) => [...prev, { q: "", a: "" }]);
-  }
-
-  function deleteQuestion(index: number) {
-    setQuestions((prev) => {
-      const newState = [...prev];
-      newState.splice(index, 1);
-      return newState;
-    });
-  }
-
-  function editQuestion(index: number, value: string) {
-    setQuestions((prev) => {
-      const nextState = produce(prev, (draftState) => {
-        draftState[index].q = value;
-      });
-      return nextState;
-    });
-  }
-
-  function editAnswer(index: number, value: string) {
-    setQuestions((prev) => {
-      const nextState = produce(prev, (draftState) => {
-        draftState[index].a = value;
-      });
-      return nextState;
-    });
-  }
-
   return (
     <Container>
       <button onClick={() => generateTxtFile(teamBlue, teamRed, questions)}>
         Generate .txt Document
       </button>
+      <hr />
       <br />
-      <br />
-      <LoadTeams setTeamRed={setTeamRed} setTeamBlue={setTeamBlue} />
-      <br />
-      <br />
-      <LoadQuestions setQuestions={setQuestions} />
-      <br />
-      <br />
-      <h3>Blaues Team</h3>
-      <StudentList
-        students={teamBlue}
-        changeStudentName={changeTeamBlue}
-        deleteStudent={deleteStudent(setTeamBlue)}
-        toggleStudentTeam={toggleStudentTeam}
-        toggleDirection={"fromBlueToRed"}
+      <StudentEditorView
+        setTeamRed={setTeamRed}
+        setTeamBlue={setTeamBlue}
+        teamBlue={teamBlue}
+        teamRed={teamRed}
       />
-      <div>
-        <button onClick={() => addStudent(setTeamBlue)}> + </button>
-      </div>
+      <hr />
       <br />
-      <h3>Rotes Team</h3>
-      <StudentList
-        students={teamRed}
-        changeStudentName={changeTeamRed}
-        deleteStudent={deleteStudent(setTeamRed)}
-        toggleStudentTeam={toggleStudentTeam}
-        toggleDirection={"fromRedToBlue"}
-      />
-      <div>
-        <button onClick={() => addStudent(setTeamRed)}> + </button>
-      </div>
-      <h3>Fragen und Antworten</h3>
-      <QuestionList
-        questions={questions}
-        deleteQuestion={deleteQuestion}
-        editQuestion={editQuestion}
-        editAnswer={editAnswer}
-      />
-      <div>
-        <button onClick={addQuestion}> + </button>
-      </div>
+      <QuestionEditorView setQuestions={setQuestions} questions={questions} />
       <br />
       <br />
     </Container>
---
+++ src/StudentEditorView.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { LoadTeams } from "./LoadTeams";
+import { StudentList } from "./StudentList";
+import { produce } from "immer";
+import { toggleDirections } from "./interfaces";
+
+interface IProps {
+  setTeamRed: Function;
+  setTeamBlue: Function;
+  teamBlue: string[];
+  teamRed: string[];
+}
+
+export function StudentEditorView({
+  setTeamBlue,
+  teamBlue,
+  setTeamRed,
+  teamRed,
+}: IProps): JSX.Element {
+  function deleteStudent(teamSetter: Function) {
+    return function (index: number) {
+      teamSetter((baseState: string[]) => {
+        const nextState = produce(baseState, (draftState: string[]) => {
+          draftState.splice(index, 1);
+        });
+        return nextState;
+      });
+    };
+  }
+
+  function addStudent(teamSetter: Function, studentName: string = "") {
+    teamSetter((prev: string[]) => [...prev, studentName]);
+    console.debug(teamBlue);
+  }
+
+  function toggleStudentTeam(index: number, direction: toggleDirections) {
+    function toggle(
+      index: number,
+      fromTeam: string[],
+      fromTeamSetter: Function,
+      toTeamSetter: Function
+    ) {
+      addStudent(toTeamSetter, fromTeam[index]);
+      deleteStudent(fromTeamSetter)(index);
+    }
+    switch (direction) {
+      case "fromBlueToRed":
+        toggle(index, teamBlue, setTeamBlue, setTeamRed);
+        break;
+      case "fromRedToBlue":
+        toggle(index, teamRed, setTeamRed, setTeamBlue);
+        break;
+      default:
+        throw new Error("Unknown student change direction");
+    }
+  }
+
+  //// ---------------------------------------------------------------------------------------------
+  //// TODO deduplicate and rename
+  function changeTeamBlue(studentname: string, index: number) {
+    setTeamBlue((baseState: string[]) => {
+      const nextState = produce(baseState, (draftState: string[]) => {
+        draftState[index] = studentname;
+      });
+      return nextState;
+    });
+  }
+  function changeTeamRed(studentname: string, index: number) {
+    setTeamRed((baseState: string[]) => {
+      const nextState = produce(baseState, (draftState: string[]) => {
+        draftState[index] = studentname;
+      });
+      return nextState;
+    });
+  }
+  //// ---------------------------------------------------------------------------------------------
+
+  return (
+    <>
+      <br />
+      <LoadTeams setTeamRed={setTeamRed} setTeamBlue={setTeamBlue} />
+      <br />
+      <h3>Blaues Team</h3>
+      <StudentList
+        students={teamBlue}
+        changeStudentName={changeTeamBlue}
+        deleteStudent={deleteStudent(setTeamBlue)}
+        toggleStudentTeam={toggleStudentTeam}
+        toggleDirection={"fromBlueToRed"}
+      />
+      <div>
+        <button onClick={() => addStudent(setTeamBlue)}> + </button>
+      </div>
+      <br />
+      <h3>Rotes Team</h3>
+      <StudentList
+        students={teamRed}
+        changeStudentName={changeTeamRed}
+        deleteStudent={deleteStudent(setTeamRed)}
+        toggleStudentTeam={toggleStudentTeam}
+        toggleDirection={"fromRedToBlue"}
+      />
+      <div>
+        <button onClick={() => addStudent(setTeamRed)}> + </button>
+      </div>
+    </>
+  );
+}
